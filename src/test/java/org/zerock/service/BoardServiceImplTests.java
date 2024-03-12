715d4b6c5e4f02fc32a8f700761806787053827d
kty <kwonabraham@naver.com>
2024-03-12 11:07:11 +0900

    regist update JUnit 테스트 확인

---
+++ src/test/java/org/zerock/service/BoardServiceImplTests.java
@@ -0,0 +1,45 @@
+package org.zerock.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceImplTests {
+
+  //4.3 이상부터는 Autowired 한 개 이상일때 할 필요없음 
+  //Test에서는 해야됨
+  @Autowired
+  private BoardService service;
+  
+  @Test
+  public void testExist() {
+    log.info(service);
+    
+    //null값 아닌지 확인
+    assertNotNull(service);
+  }
+  
+  @Test
+  public void testRegister() {
+  
+    BoardVO board =new BoardVO();
+    
+    board.setTitle("새로 작성하는 글 selectKey-ser");
+    board.setContent("새로 작성하는 글 내용 selectKey-ser");
+    board.setWriter("newbie-ser");
+    
+    service.register(board);
+    
+    log.info("생성된 게시물의 번호 : " + board.getBno());
+  }
+}
