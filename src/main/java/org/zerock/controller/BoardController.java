4918df407fa2a45cbbcebccfe72215fed09b10c2
kty <kwonabraham@naver.com>
2024-03-22 14:45:44 +0900

    코드 작업중

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -91,6 +91,8 @@ public class BoardController {
 
    rttr.addAttribute("pageNum", cri.getPageNum());
    rttr.addAttribute("amount", cri.getAmount());
+   rttr.addAttribute("type", cri.getType());
+   rttr.addAttribute("keyword", cri.getKeyword());
    
    return "redirect:/board/list";
  }
@@ -107,6 +109,8 @@ public class BoardController {
    
    rttr.addAttribute("pageNum", cri.getPageNum());
    rttr.addAttribute("amount", cri.getAmount());
+   rttr.addAttribute("type", cri.getType());
+   rttr.addAttribute("keyword", cri.getKeyword());
    
    return "redirect:/board/list";    
  }
